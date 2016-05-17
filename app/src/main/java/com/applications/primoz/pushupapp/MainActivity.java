f06f4731c62e7919ba24bbf339cbe369a2396433
WinterChilly <primoz.pesjak@gmail.com>
2016-05-17 09:36:10 +0200

    1. Commit

---
+++ app/src/main/java/com/applications/primoz/pushupapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.applications.primoz.pushupapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    int test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
