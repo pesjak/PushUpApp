58888751e08e9f65f48be7c6c7c9fcde49559446
WinterChilly <primoz.pesjak@gmail.com>
2016-05-19 13:06:15 +0200

    Splash Intro

--- app/src/main/java/com/applications/primoz/pushupapp/MainActivity.java
+++ app/src/main/java/com/applications/primoz/pushupapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.applications.primoz.pushupapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    int test;
 
+    //TODO Preveri, če je prvič prižgal App
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showSelectGoal();
+    }
+
+    private void showSelectGoal() {
+        FragmentPrvic fragmentPrvic = new FragmentPrvic();
+        fragmentPrvic.setArguments(getIntent().getExtras());
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(android.R.id.content, fragmentPrvic)
+                .commit();
     }
 }
